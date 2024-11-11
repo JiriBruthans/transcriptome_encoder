93c00b10c36b19450d411558d570dddb09bf535b
Jiri Bruthans <jiribruthans@zia1.metacentrum.cz>
2024-11-11 16:32:30 +0100

    some fixes to autoprepare

--- prepare_workspace.sh
+++ prepare_workspace.sh
@@ -6,8 +6,8 @@ cd $SCRATCHDIR
 # Clone repository
 git clone https://github.com/JiriBruthans/transcriptome_encoder.git
 cd transcriptome_encoder
-cp /storage/praha1/home/jiribruthans/transcriptome_encoder/embedding_layer.pt .
-cp /storage/praha1/home/jiribruthans/transcriptome_encoder/gene_names.pt .
+cp /storage/praha1/home/jiribruthans/sota/transcriptome_encoder/embedding_layer.pt .
+cp /storage/praha1/home/jiribruthans/sota/transcriptome_encoder/gene_names.pt .
 
 # Download and install Miniforge non-interactively
 wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh
@@ -19,4 +19,4 @@ eval "$($SCRATCHDIR/miniforge3/bin/conda shell.bash hook)"
 
 # Create and activate environment
 mamba env create -f environment.yml
-mamba activate transcriptome_encoder
+conda activate txn-enc
