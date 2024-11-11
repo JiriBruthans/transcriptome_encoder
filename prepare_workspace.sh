961364d499e5b37a1f701a74fdf2fb96e7446fbf
JiriBruthans <136194089+JiriBruthans@users.noreply.github.com>
2024-11-11 16:13:10 +0100

    Update prepare_workspace.sh

--- prepare_workspace.sh
+++ prepare_workspace.sh
@@ -6,6 +6,8 @@ cd $SCRATCHDIR
 # Clone repository
 git clone https://github.com/JiriBruthans/transcriptome_encoder.git
 cd transcriptome_encoder
+cp /storage/praha1/home/jiribruthans/transcriptome_encoder/embedding_layer.pt .
+cp /storage/praha1/home/jiribruthans/transcriptome_encoder/gene_names.pt .
 
 # Download and install Miniforge non-interactively
 wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh
